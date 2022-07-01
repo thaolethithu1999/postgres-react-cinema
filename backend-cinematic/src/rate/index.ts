c7a601561421140456f721868a1c5df9b2f0456b
Thao Le Thi Thu <ltthuthao@tma.com.vn>
2022-07-01 17:57:50 +0700

    upload review

--- backend-cinematic/src/rate/index.ts
+++ backend-cinematic/src/rate/index.ts
@@ -7,9 +7,12 @@ import { Rate, RateFilter, RateId, rateModel, RateRepository, RateService } from
 import { RateController } from './rate-controller';
 import { SqlRateRepository } from './sql-rate-repository';
 import { SqlInfoRepository } from '../cinema/sql-info-repository';
+import { Info } from '../cinema/cinema';
+
 export * from './rate-controller';
 export * from './rate';
 export { RateController };
+
 
 
 export class RateManager extends Manager<Rate, RateId, RateFilter> implements RateService {
@@ -26,10 +29,7 @@ export class RateManager extends Manager<Rate, RateId, RateFilter> implements Ra
         const id = rate.id;
         const userId = rate.userId || '';
         const rateId: RateId = { id, userId };
-        
-        const dbRate = this.repository.load(rateId);
-        console.log(dbRate);
-        
+
         let info = await this.infoRepository.load(rate.id);
         if (!info) {
             let dbInfo = {
@@ -134,4 +134,29 @@ export function useRateController(log: Log, db: DB, mapper?: TemplateMap): RateC
     return new RateController(log, useRateService(db, mapper),);
 }
 
+
+// let info = await this.infoRepository.load(rate.id);
+// if (!info) {
+//     let dbInfo = {
+//         'id': rate.id,
+//         'rate': 0,
+//         'rate1': 0,
+//         'rate2': 0,
+//         'rate3': 0,
+//         'rate4': 0,
+//         'rate5': 0,
+//         'viewCount': 0,
+//     };
+//     await this.infoRepository.insert(dbInfo);
+//     info = await this.infoRepository.load(rate.id);
+// }
+// if (!info || typeof info[('rate' + rate.rate?.toString()) as keyof Info] === 'undefined') {
+//     return false;
+// }
+// const dbRate = this.repository.load(rateId);
+// console.log(dbRate);
+
+// if(!dbRate){
+//     const res = await this.repository.update(rate);
+// }
 
